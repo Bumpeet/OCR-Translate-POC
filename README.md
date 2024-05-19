423a0596d223dd3972f2616caa8cf5472cde46b9
Bumpeet <pavanchetan81@gmail.com>
2024-05-19 18:38:01 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # OCR-Translate-POC
+
+Prerequisites:
+    - Install the executable of tesseract OCR from this (link)[https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-5.3.3.20231005.exe]
+    - Durning installation also include the swedish installation.
+    - After installation add the path of executable to the environment variables, Path variable.
+
+All the images are present in the images folder in the root directory. Just follow the below commands.
+
+Python:
+    - create a venv using `python -m venv ocr-translate`
+    - activate the environment `~\ocr-translate\Scripts\activate`
+    - then run this command `pip install -m requirements.exe`
+    - run `python main.py`
---
+++ main.py
@@ -0,0 +1,31 @@
+from transformers import MarianMTModel, MarianTokenizer
+from PIL import Image
+import pytesseract
+from pathlib import Path
+
+print("... loading the translation model")
+model_name = "Helsinki-NLP/opus-mt-sv-en"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
+print("... reading the contents")
+# Open an image file
+# image = Image.open(r'D:\\Chetan\\passion\\tesseract-poc\\images\\img3.png')
+
+base_dir = Path(r'D:\\Chetan\\passion\\tesseract-poc\\images')
+
+# Open an image file
+# image = Image.open(r'D:\\Chetan\\passion\\tesseract-poc\\images\\MicrosoftTeams-image (11).png')
+
+for img_path in base_dir.glob('*.png'):
+    # Use Tesseract to do OCR on the image
+    img = Image.open(img_path)
+    text = pytesseract.image_to_string(img, lang='swe')
+
+    # Use Tesseract to do OCR on the image
+    # text = pytesseract.image_to_string(image, lang='swe')
+    print("swedish --> \n", text)
+
+    print("... converting from swedish to english ")
+    translated = model.generate(**tokenizer(text, return_tensors="pt", padding=True))
+    print("english --> \n", [tokenizer.decode(t, skip_special_tokens=True) for t in translated])
