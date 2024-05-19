0af1707a279296cc5cd224e6e93320d43fcec148
Venkata Sai Pavan Chetan Annam <86848360+Bumpeet@users.noreply.github.com>
2024-05-19 18:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,19 @@ from PIL import Image
 import pytesseract
 from pathlib import Path
 
-print("... loading the translation model")
+print("... loading the translation model...")
 model_name = "Helsinki-NLP/opus-mt-sv-en"
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name)
 
-print("... reading the contents")
-# Open an image file
-# image = Image.open(r'D:\\Chetan\\passion\\tesseract-poc\\images\\img3.png')
-
 base_dir = Path(r'images')
 
-# Open an image file
-# image = Image.open(r'D:\\Chetan\\passion\\tesseract-poc\\images\\MicrosoftTeams-image (11).png')
-
 for img_path in base_dir.glob('*.png'):
-    # Use Tesseract to do OCR on the image
+  
+    print("... reading the contents ...")
     img = Image.open(img_path)
-    text = pytesseract.image_to_string(img, lang='swe')
-
     # Use Tesseract to do OCR on the image
-    # text = pytesseract.image_to_string(image, lang='swe')
+    text = pytesseract.image_to_string(img, lang='swe')
     print("swedish --> \n", text)
 
     print("... converting from swedish to english ")
